ed9eea1e26bd0251c2f0d20b1e1cf4d4c027a4d5
Bill Grayson Carol Ingram Grayson Sons Ingram <grayson.ingram@att.net>
2018-07-27 12:42:42 -0500

    Added a Furniture class with attributes

---
+++ OpenHouse/Furniture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenHouse
+{
+    class Furniture
+    {
+        public string Age { get; set; }
+        public string Type { get; set; }
+        public bool IsComfortable { get; set; }
+        public string Color { get; set; }
+
+    }
+}
